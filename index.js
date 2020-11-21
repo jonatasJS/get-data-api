1ee9ff5d6eca47f65ea909a61754efbd8ddb949c
Jônatas Souza Soares <56240097+sozinhoL@users.noreply.github.com>
2020-11-21 18:02:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/:username', async (req, res) => {
                 <head>
                   <meta charset="UTF-8">
                   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                  <title>${dataUser.name}</title>
+                  <title>${dataUser.name} (${dataUser.login})</title>
                   <link rel="shortcut icon" href="${dataUser.avatarUrl}" type="image/x-icon">
                   <style>
                     body {
