4bb505edc40da72d12b97d3100f91ec109181b2b
Jônatas Souza Soares <56240097+sozinhoL@users.noreply.github.com>
2020-12-13 00:12:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,83 @@ app.get('/', (req, res) => {
   // res.send('Essa página aida está em manuntenção!<br><br><a href="/sozinhol">Clique aqui para ai para uma de exemplo!</a><br><br>Depois mude "sozinhol" por algum perfil do GitHub!')
 });
 
+app.get('/kety', (req, res) => {
+  res.send(`<!DOCTYPE html>
+                <html lang="pt">
+                <head>
+                  <meta charset="UTF-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <title>${dataUser.name} (${dataUser.login})</title>
+                  <link rel="shortcut icon" href="${dataUser.avatarUrl}" type="image/x-icon">
+                  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+                  <style>
+                    body {
+                      background: rgb(70, 142, 236);
+                      font: normal 15pt Arial;
+                    }
+                
+                    header {
+                      color: white;
+                      text-align: center;
+                    }
+                
+                    section {
+                      display: flex;
+                      gap: 30px;
+                      flex-direction: inherit;
+                      background: white;
+                      border-radius: 10px;
+                      padding: 15px;
+                      width: 750px;
+                      margin: auto;
+                      box-shadow: 5px 5px 10px rgb(0 0 0);
+                    }
+
+                    section div.img a img {
+                      min-width: 400px;
+                      min-height: 400px;
+                      width: 400px;
+                      height: 400px;
+                      max-width: 400px;
+                      max-height: 400px;
+                    }
+
+                    section div.main {
+                      background: rgba(0,0,0,0.2);
+                      padding: 20px;
+                      border-radius: 10px;
+                    }
+                
+                    footer {
+                      color: white;
+                      text-align: center;
+                      font-style: italic;
+                    }
+                  </style>
+                </head>
+                <body>
+                  <header>
+                    <h1>Olá eu sou Kettelyn Amanda</h1>
+                  </header>
+                  <section>
+                    <div class="img">
+                      <a spellcheck="false" href="${dataUser.urlApi}" rel="nofollow" target="_blank" dir="auto">
+                        <img  style="border-radius: 50%;" src="https://pps.whatsapp.net/v/t61.24694-24/129559646_3604515019597777_6562232797452939130_n.jpg?oh=69827610f7332b783a6d180b929ff816&oe=5FD9C408&t=s&u=556391187026@c.us&i=1607805286&n=Kfn8q5Qkm9wXI0lXQKJyEHZRt/xfmoJBlD41RrEavy8="/>
+                      </a>
+                    </div>
+                    <div class="main">
+                      <p>Sou linda pra Karalho!</p>
+                      <p>Não me quis antes não tem agora!</p>
+                    </div>
+                  </section>
+                  <footer>
+                    <p>&copy; <a spellcheck="false" href="https://github.com/sozinhoL" rel="nofollow" target="_blank" dir="auto">sozinhoL</a></p>
+                  </footer>
+                </body>
+                </html>`);
+});
+
 app.get('/:username', async (req, res) => {
   const userName = await req.params.username;
   
